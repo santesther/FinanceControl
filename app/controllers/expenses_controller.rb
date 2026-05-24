fee92404ee7d943a24a1e314f57df97e891317b6
esther <esthersant.edu@gmail.com>
2026-05-24 09:53:13 -0300

    feat: add frontend for expenses, category and monthly_goals

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -3,7 +3,7 @@ class ExpensesController < ApplicationController
 
   # GET /expenses or /expenses.json
   def index
-    @expenses = Expense.all
+  @expenses = current_user.expenses.includes(:category).order(date: :desc)
   end
 
   # GET /expenses/1 or /expenses/1.json
